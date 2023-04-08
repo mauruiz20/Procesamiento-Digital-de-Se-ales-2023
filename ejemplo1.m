c6095bd1e92e7d26148c3cd76707c9b9a35628e2
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2023-04-08 20:28:46 -0300

    feat: ejemplos

---
+++ ejemplo1.m
@@ -0,0 +1,7 @@
+%ejemplo1 archivo .m
+gomas=4;
+cintas=6;
+lapices=2;
+cantidad=gomas+cintas+lapices %numero de items comprados
+costo=gomas*25+cintas*22+lapices*30 %costo total
+costo_promedio=costo/cantidad
